d19fe143d9fc418dff1594806c4ca52eb9165429
FDS1990 <f.disales@hotmail.com>
2018-10-28 19:01:31 +0100

    Update merge_data.sql

--- merge_data.sql
+++ merge_data.sql
@@ -1,36 +1,46 @@
 declare
 
-cursor studente_cursor is select * from studente;
-
-type studente_record is record
-(
-
- matricola varchar2(50),
- nome varchar2(50),
- cognome varchar2(50)
-
-);
-studente_dati studente_record;
-
+i integer;
+j integer;
+nodata exception;
+cursor ctrl_bk is select * from studentebk;
 
 begin
 
- open studente_cursor;
+for i in(select * from studente) loop
+ 
+open ctrl_bk;
+dbms_output.put_line(ctrl_bk%rowcount);
 
- loop
-  
- fetch studente_cursor into studente_dati.matricola,studente_dati.nome,studente_dati.cognome;
 
-MERGE INTO studente e USING studentebk bk ON (e.matricola = bk.matricola)
+if(ctrl_bk%rowcount=0)then
+raise nodata;
+else
+for j in(select * from studentebk) loop
+ if(i.matricola=j.matricola) then
+  dbms_output.put_line('inserito nel ramo update');
+  update studentebk set nome=i.nome,cognome=i.cognome where matricola=i.matricola;
+  else
+  dbms_output.put_line('inserito nel ramo insert');
+  insert into studentebk values(i.matricola,i.nome,i.cognome);
+  end if;
 
-  WHEN MATCHED THEN update SET e.nome=studente_dati.nome,e.cognome=studente_dati.cognome where e.matricola=bk.matricola 
-  WHEN NOT MATCHED THEN insert(e.matricola,e.nome,e.cognome) values(studente_dati.matricola,studente_dati.nome,studente_dati.cognome); 
+  end loop;
+end if;
 
-exit when studente_cursor%notfound;
-	
-   
 end loop;
 
-close studente_cursor;
+close ctrl_bk;
+
+exception when nodata then
+
+dbms_output.put_line('inserito nel ramo nodata');
+  for i in (select * from studente) loop
+  insert into studentebk values(i.matricola,i.nome,i.cognome);
+  end loop; 
+ 
+  when others then
+    dbms_output.put_line('eccezzione'); 
+
 
 end;
